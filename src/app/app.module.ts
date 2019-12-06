3a6bd7387c0c427164e9ed9f2d63b76aeed03a48
Aaron Isaacs <aaron.isaacs.ing@gmail.com>
2019-12-05 23:25:15 -0500

    Primeros pasos en angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DirectivaComponent } from './components/directiva/directiva.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DirectivaComponent,
+    ClientesComponent
   ],
   imports: [
     BrowserModule
